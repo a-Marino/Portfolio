62260919952a73f497228a7b1d065081bf4a89e8
a-Marino <andresmarinolaiolo@hotmail.com.ar>
2021-05-26 19:14:36 -0300

    sociales index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,13 +3,13 @@
         <Nav/>
         <div class="redes absolute bottom-0 right-0">
             <div class="flex space-x-4 p-4">
-                <router-link to='/instagram'>
+                <router-link to='/instagram' class="red-social">
                     <img src="@/assets/images/instagram.png" class="w-8">
                 </router-link>
-                <router-link to='/github'>
+                <router-link to='/github' class="red-social">
                     <img src="@/assets/images/github.png" class="w-8">
                 </router-link>
-                <router-link to='/linkedin'>
+                <router-link to='/linkedin' class="red-social">
                     <img src="@/assets/images/linkedin.png" class="w-8">
                 </router-link>
             </div>
